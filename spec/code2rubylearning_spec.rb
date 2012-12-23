eda8f750d88e522d370dbd819fa06192c34b1a3d
Rudi Luzar <rudi@rudicode.com>
2012-12-23 07:59:02 -0500

    started with the specs

--- spec/code2rubylearning_spec.rb
+++ spec/code2rubylearning_spec.rb
@@ -1,7 +1,12 @@
 require "spec_helper"
 
 describe Code2rubylearning do
- it"should skip this minispec"
- it"should skip this minispec too"
- 
+
+  describe "FileHandling" do
+
+    it "should " 
+    it"should skip this minispec too"
+
+  end
+
 end
