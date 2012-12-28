60f223361b1f148f554ee9afcfc65efcfe3b8e39
Rudi Luzar <rudi@rudicode.com>
2012-12-28 16:10:45 -0500

    Version bunmped to 0.1.0

--- lib/code2rubylearning/version.rb
+++ lib/code2rubylearning/version.rb
@@ -1,3 +1,3 @@
 module Code2rubylearning
-  VERSION = "0.0.1"
+  VERSION = "0.1.0"
 end
--- spec/code2rubylearning/parse_cli_spec.rb
+++ spec/code2rubylearning/parse_cli_spec.rb
@@ -8,6 +8,7 @@ describe "ParseCLI" do
   describe ".parse" do
     it "should parse command line into files and options" do
       cmd_line         = ['file1.rb', '-b', '--help', 'file2.rb', 'file3.txt']
+
       expected_files   = ['file1.rb', 'file2.rb', 'file3.txt']
       expected_options = ['-b', '--help']
       ParseCLI.parse cmd_line
