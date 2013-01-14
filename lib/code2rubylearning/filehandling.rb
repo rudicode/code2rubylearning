3d2f34cc64d19d725d1e4111ebcd3ef4fa988281
Rudi Luzar <rudi@rudicode.com>
2013-01-14 14:25:03 -0500

    Refactoring instance variables in FileHandling

--- lib/code2rubylearning/filehandling.rb
+++ lib/code2rubylearning/filehandling.rb
@@ -4,21 +4,18 @@ module Code2rubylearning
     attr_accessor :data, :name, :type
 
     def initialize(file_name, options = { })
-      @name = nil
-      @data = ""
-      @type = ""
-       # load_data file_name
+      self.name = nil
+      self.type = ""
       self.data = file_name
-      identify_file_type if self.name
+      identify_file_type if name
     end
 
-    # def load_data file_name
     def data=(file_name)
       @data = ""
       if file_name
         if File.exists?(file_name)
-          @name = file_name
-          @data = IO.read(@name)
+          self.name = file_name
+          @data = IO.read(name)
         end
       end
     end
@@ -28,7 +25,7 @@ module Code2rubylearning
     # if extension fails try checking the first line
     # it may contain #! and give a clue.
     def identify_file_type
-      self.type = "text"  #NOTE why do we need self.type= why does type= not work
+      self.type = "text"
       
       data_lines = data.split("\n")
 
